6c18aff7e91a21d689a5f73a60091a68b19f272a
SURYA-200346 <surya.jsdev@gmail.com>
2025-08-22 13:39:46 +0530

    array & arr  method

--- array.js
+++ array.js
@@ -1,20 +1,22 @@
-const num=[6,5,4,3,2,1]
-const fruits=["apple","Banana","Watermelon",]
-console.log(num);
-console.log(num[2]);
-console.log(num.push(7));
-console.log(num.pop(6));
-console.log(num.unshift(0));
-console.log(num.shift(1));
-console.log(num.sort());
-console.log(fruits.reverse());
-// console.log(fruits.splice(2));
-console.log(fruits.fill("pineapple",1));
+// const name=["surya","vijay","mani","vicky"]
+// name.push("Balaji","Parasanna");
+// // name.pop();
+// console.log(name);
 
 
 
+// const num=[10,9,8,7,6,5,4,3,2,1]
+//  num.sort((a, b) => b- a); 
+// console.log(...num);
 
+const student = [{ name: "balaji", mark: 67 },
+{ name: "surya", mark: 100 },
+{ name: "vicky", mark: 89 }]
 
+const rank = student.sort((a, b) => b.mark - a.mark);
 
+rank.forEach((student, index) => {
+    console.log(`rank ${index + 1} :${student.name} - ${student.mark} marks`);
 
+})
 
