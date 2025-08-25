30ba2081e9935f93a41cabc977ec9b5e1432622a
SURYA-200346 <surya.jsdev@gmail.com>
2025-08-25 22:00:29 +0530

    get request

---
+++ get_request.js
@@ -0,0 +1,30 @@
+// function deleteuser(){
+//   fetch("https://68a582352a3deed2960dbd2c.mockapi.io/form/5", {
+//     method: "PUT",
+//   })
+//     .then((res) => {
+//       if (res.ok){   
+//         console.log("User deleted successfully");
+//       } else {
+//         console.log("Failed to delete user");
+//       }
+//     })
+//     .catch((error) => {
+//       console.log("Error:", error);
+//     });
+// }
+// deleteuser();
+
+// function add(){
+//     var a=10;
+//     var b=10;
+//     console.log(a+b);
+// }
+// add()
+
+const sum=()=>{
+let a=10;
+let b=30;
+console.log(a+b);
+}
+sum()
